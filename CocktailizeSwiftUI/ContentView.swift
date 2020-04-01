98d682edf65287935ebb4d5c8ecdbbde13acdbe8
Anthony Loroscio <anthonyloroscio@gmail.com>
2020-04-01 11:38:57 +0200

    Initial Commit

---
+++ CocktailizeSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CocktailizeSwiftUI
+//
+//  Created by anthony loroscio on 01/04/2020.
+//  Copyright © 2020 anthony loroscio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
